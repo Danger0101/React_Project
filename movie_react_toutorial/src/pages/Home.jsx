b639007e0f6f4b0a664b5e320e5ee4dacf6a5f42
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-02-06 14:16:42 +0000

    Set up a basic useState and Prevent page reload

--- movie_react_toutorial/src/pages/Home.jsx
+++ movie_react_toutorial/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState('');
+
   const movies = [
     { id: 1, title: "The Potato", release_date: 1994 },
     { id: 2, title: "The Onion", release_date: 1990, },
@@ -10,8 +13,26 @@ function Home() {
     { id: 6, title: "The Peanut", release_date: 1989 },
     { id: 7, title: "The Chesse", release_date: 2020 },
     { id: 8, title: "The Banana", release_date: 2023 },];
+
+    const handleSearch = () => {
+        e.preventDefault();
+        alert(`Search for ${searchQuery}`);
+        setSearchQuery('');
+    };
+
   return (
     <div className="home">
+        <form onSubmit={handleSearch} className="seach-form">
+            <input
+                type="text"
+                placeholder="Search For Movies..."
+                className="search-input"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="search-button">Search</button>
+        </form>
+
         <div className="movies-grid">
             {movies.map(movie => (
                 <MovieCard movie={movie} key={movie.id} />
