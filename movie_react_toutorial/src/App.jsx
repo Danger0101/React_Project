96d4c56d8c27081b719b35cf4b4fc7b38ea441eb
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-02-06 14:54:56 +0000

    Added in Navbar element

--- movie_react_toutorial/src/App.jsx
+++ movie_react_toutorial/src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import Home from './pages/Home'
+import Favorite from './pages/Favorites'
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorite />} />
+      </Routes>
+    </div>
   )
 }
-
 
 export default App
